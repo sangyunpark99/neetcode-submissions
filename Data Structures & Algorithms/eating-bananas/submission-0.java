5355656ef9a2e576f0f503d385f3914ad24d2ad7
박상윤 <96441638+sangyunpark99@users.noreply.github.com>
2026-03-31 18:14:40 +0900

    Add: eating-bananas - submission-0

---
+++ Data Structures & Algorithms/eating-bananas/submission-0.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int minEatingSpeed(int[] piles, int h) {
+        int left = 1;
+        int right = 1_000_000_000;
+        int answer = 0;
+
+        while(left <= right) {
+            int mid = (left + right) / 2;
+
+            if(check(piles, mid, h)) {
+                right = mid - 1;
+                answer = mid;
+            }else {
+                left = mid + 1;
+            }
+        }
+
+        return answer;
+    }
+
+    private boolean check(int[] piles, int k, int h) { // 시간
+        int time = 0;
+        for(int pile : piles) {
+            int v = pile / k;
+            time += v == 0 ? 1 : v;
+            if(v > 0 && pile % k > 0) {
+                time++;
+            }
+            if(time > h) return false;
+        }
+
+        return time <= h;
+    }
+}
