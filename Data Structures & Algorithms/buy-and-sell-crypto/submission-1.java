59ffc2bc59d1f52285ad901ac61cebb3cd3ba41c
박상윤 <96441638+sangyunpark99@users.noreply.github.com>
2026-04-01 17:36:40 +0900

    Add: buy-and-sell-crypto - submission-1

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-1.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int answer = 0;
+        int left = 0;
+        int right = 1;
+
+        while(right < prices.length) {
+            if(prices[left] < prices[right]) { // 증가하는 구조
+                answer = Math.max(answer, prices[right] - prices[left]);
+                right++;
+            }else { // 감소하는 구조면 구역 이동
+                left = right;
+                right = left + 1;
+                continue;
+            }
+        }
+
+        return answer;
+    }
+}
