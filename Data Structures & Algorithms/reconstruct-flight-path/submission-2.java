2abbc095797546ccd0964479da289966f360eea4
박상윤 <96441638+sangyunpark99@users.noreply.github.com>
2026-04-06 13:46:57 +0900

    Add: reconstruct-flight-path - submission-2

---
+++ Data Structures & Algorithms/reconstruct-flight-path/submission-2.java
@@ -0,0 +1,57 @@
+class Solution {
+
+    private HashMap<String, List<String>> paths = new HashMap<>();
+    private HashMap<String, Integer> visited = new HashMap<>(); // 동일한 비행기 티켓
+    private List<String> ans = new ArrayList<>();
+
+    public List<String> findItinerary(List<List<String>> tickets) {
+
+        for(int i = 0; i < tickets.size(); i++) {
+            String from = tickets.get(i).get(0);
+            String to = tickets.get(i).get(1);
+
+            if(!paths.containsKey(from)) {
+                paths.put(from, new ArrayList<>());
+            }
+
+            paths.get(from).add(to);
+            String key = from + "_" + to;
+            visited.put(key, visited.getOrDefault(key, 0) + 1);
+        }
+
+        for(String key: paths.keySet()) {
+            Collections.sort(paths.get(key));
+        }
+
+        List<String> start = new ArrayList<>();
+        start.add("JFK");
+        dfs("JFK", start, tickets.size());
+
+        return ans;
+    }
+
+    private boolean dfs(String cur, List<String> answer, int totalTickets) { // 정답인 경로
+
+        if(answer.size() == totalTickets + 1) {
+            ans = answer;
+            return true;
+        }
+
+        if(!paths.containsKey(cur)) {
+            return false;
+        }
+
+        for(String next: paths.get(cur)) {
+            String key = cur + "_" + next;
+            if(visited.get(key) >= 1) {
+                visited.put(key, visited.get(key) - 1);
+                answer.add(next);
+                if(dfs(next, answer, totalTickets)) return true;
+                answer.remove(answer.size() - 1);
+                visited.put(key, visited.get(key) + 1);
+            }
+        }
+
+        return false;
+    }
+}
